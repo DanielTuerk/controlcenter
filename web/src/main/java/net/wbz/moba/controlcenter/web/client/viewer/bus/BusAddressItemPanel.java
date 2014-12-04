6588dd32d1c3733ecc865cb6eb629166bda8af55
Aiperi Baizakova <aiperibaizakova@community4you.de>
2014-12-04 16:06:22 +0100

    delayed logic for color of button is done with scheduling

--- web/src/main/java/net/wbz/moba/controlcenter/web/client/viewer/bus/BusAddressItemPanel.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/client/viewer/bus/BusAddressItemPanel.java
@@ -1,89 +1,122 @@
 package net.wbz.moba.controlcenter.web.client.viewer.bus;
-
-import java.util.HashMap;
-
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FlowPanel;
 
+import de.novanic.eventservice.client.event.listener.RemoteEventListener;
 import net.wbz.moba.controlcenter.web.client.ServiceUtils;
 import net.wbz.moba.controlcenter.web.client.util.EmptyCallback;
-
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.Label;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Daniel Tuerk (daniel.tuerk@w-b-z.com)
  */
-public class BusAddressItemPanel extends org.gwtbootstrap3.client.ui.gwt.FlowPanel {
-    private final FlowPanel dataPanel = new FlowPanel();
-    private final Map<Integer, Button> bitButtons = new HashMap<>();
+public class BusAddressItemPanel extends
+		org.gwtbootstrap3.client.ui.gwt.FlowPanel {
+	private final FlowPanel dataPanel = new FlowPanel();
+	private final Map<Integer, Button> bitButtons = new HashMap<>();
+	private RemoteEventListener listener;
+	static boolean isWorking = false;
 
-    public BusAddressItemPanel(final int busNr,final int address) {
-        getElement().getStyle().setFloat(Style.Float.LEFT);
-        Label lblAddress = new Label(String.valueOf(address));
-        lblAddress.getElement().getStyle().setColor("BLUE");
-        lblAddress.getElement().getStyle().setFloat(Style.Float.LEFT);
+	public BusAddressItemPanel(final int busNr, final int address) {
+		getElement().getStyle().setFloat(Style.Float.LEFT);
+		Label lblAddress = new Label(String.valueOf(address));
+		lblAddress.getElement().getStyle().setBackgroundColor("LightGray");
+		lblAddress.getElement().getStyle().setFloat(Style.Float.LEFT);
+		lblAddress.getElement().getStyle().setWidth(20, Unit.PX);
+		lblAddress.getElement().getStyle().setHeight(48, Unit.PX);
+		lblAddress.getElement().getStyle().setPaddingTop(20, Unit.PX);
+		lblAddress.getElement().getStyle().setPaddingBottom(20, Unit.PX);
+		lblAddress.getElement().getStyle().setPaddingRight(0.1, Unit.PX);
+		lblAddress.getElement().getStyle().setPaddingLeft(0.1, Unit.PX);
+		lblAddress.getElement().getStyle().setColor("DarkGray");
+		lblAddress.getElement().getStyle().setBorderStyle(BorderStyle.SOLID);
+		lblAddress.getElement().getStyle().setBorderWidth(0.4, Unit.PX);
+
 		add(lblAddress);
-        
-        
-        
-        for (int i = 8; i >0; i--) {
-        	 FlowPanel bitPanel = new FlowPanel();
-        	 FlowPanel bitPanelLabel = new FlowPanel();
-        	 FlowPanel bitPanelButton = new FlowPanel();
-        	 bitPanel.getElement().getStyle().setFloat(Style.Float.LEFT);
-        	 bitPanel.add(bitPanelLabel);
-        	 bitPanel.add(bitPanelButton);
-        	 add(bitPanel);
-        	 
-        	 Label lbl = new Label(String.valueOf(i));
-        	 bitPanelLabel.add(lbl);
-       
-        	final Button btns = new Button("0");
-        	btns.getElement().getStyle().setPaddingTop(3, Unit.PX);
-        	btns.getElement().getStyle().setPaddingBottom(3, Unit.PX);
-        	btns.getElement().getStyle().setPaddingLeft(6, Unit.PX);
-        	btns.getElement().getStyle().setPaddingRight(6, Unit.PX);
-        	bitButtons.put(i, btns);
-        	 
-             
-        	bitPanelButton.add(btns); 
-           
-           final int bitNr=i;
-           btns.addClickHandler(new ClickHandler(){
+		for (int i = 8; i > 0; i--) {
+			FlowPanel bitPanel = new FlowPanel();
+			FlowPanel bitPanelLabel = new FlowPanel();
+			FlowPanel bitPanelButton = new FlowPanel();
+			bitPanel.getElement().getStyle().setFloat(Style.Float.LEFT);
+			bitPanel.add(bitPanelLabel);
+			bitPanel.add(bitPanelButton);
+			add(bitPanel);
 
-			@Override
-			public void onClick(ClickEvent event) {
-				
-				ServiceUtils.getBusService().sendBusData(busNr, address, bitNr, btns.getText().equals("0") ? true : false, new EmptyCallback<Void>());
-				
-				
-			}
+			Label lbl = new Label(String.valueOf(i));
+			bitPanelLabel.add(lbl);
+			lbl.getElement().getStyle().setPaddingTop(5, Unit.PX);
+			lbl.getElement().getStyle().setPaddingBottom(5, Unit.PX);
+			lbl.getElement().getStyle().setPaddingLeft(8, Unit.PX);
+			lbl.getElement().getStyle().setPaddingRight(8, Unit.PX);
+			lbl.getElement().getStyle().setBackgroundColor("MediumPurple");
+
+			final Button btns = new Button("0");
+			btns.getElement().getStyle().setPaddingTop(3, Unit.PX);
+			btns.getElement().getStyle().setPaddingBottom(3, Unit.PX);
+			btns.getElement().getStyle().setPaddingLeft(6, Unit.PX);
+			btns.getElement().getStyle().setPaddingRight(6, Unit.PX);
+			bitButtons.put(i, btns);
+
+			bitPanelButton.add(btns);
+
+			final int bitNr = i;
+			btns.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+
+					ServiceUtils.getBusService().sendBusData(busNr, address,
+							bitNr, btns.getText().equals("0") ? true : false,
+							new EmptyCallback<Void>());
+				}
 			});
-           
-           
-        }
-        add(dataPanel);
-        
-    }
 
-    public void updateData(int data) {
+		}
+		add(dataPanel);
 
-        BigInteger bits = new BigInteger(String.valueOf(data));
-        // check the bits of each address and show it in revert order for the widgets
-        
-        for (int i = 0; i < 8; i++) {
-        	
-        	bitButtons.get(i+1).setText(String.valueOf(bits.testBit(i) ? 1 : 0));
-        	
-        	// TODO set color of button
-        	setStyleName("green-btn");
-        }
-    }
-}
+	}
+
+	public void updateData(int data) {
+		BigInteger bits = new BigInteger(String.valueOf(data));
+		// check the bits of each address and show it in revert order for the
+		// widgets
+		for (int i = 0; i < 8; i++) {
+			String dataBtn = bitButtons.get(i + 1).getText();
+			bitButtons.get(i + 1).setText(
+					String.valueOf(bits.testBit(i) ? 1 : 0));
+			if (!dataBtn.equals(bitButtons.get(i + 1).getText())) {
+                 
+				setColor(bitButtons.get(i + 1));
+			}
+		}
+	}
+	public void setColor(final Button btn){
+		 if(isWorking){
+		 return;
+		 }
+		 isWorking= true;
+		final String originalColor = btn.getElement().getStyle()
+				.getBackgroundColor();
+		btn.getElement().getStyle().setBackgroundColor("LightSteelBlue ");
+		  Timer timer = new Timer() {
+			@Override
+			public void run() {
+				btn.getElement().getStyle().setBackgroundColor(originalColor);
+				BusAddressItemPanel.isWorking = false;
+			}
+		};
+		timer.schedule(2000);
+	
+	
+	}
+	}
--- web/src/main/java/net/wbz/moba/controlcenter/web/client/viewer/bus/BusMonitorPanel.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/client/viewer/bus/BusMonitorPanel.java
@@ -1,18 +1,29 @@
 package net.wbz.moba.controlcenter.web.client.viewer.bus;
 
+import net.wbz.moba.controlcenter.web.client.util.EmptyCallback;
+
 import com.google.common.collect.Maps;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Float;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+
 import de.novanic.eventservice.client.event.Event;
 import de.novanic.eventservice.client.event.listener.RemoteEventListener;
 import net.wbz.moba.controlcenter.web.client.EventReceiver;
 import net.wbz.moba.controlcenter.web.client.ServiceUtils;
-import net.wbz.moba.controlcenter.web.client.util.EmptyCallback;
 import net.wbz.moba.controlcenter.web.shared.bus.BusDataEvent;
 import net.wbz.moba.controlcenter.web.shared.bus.DeviceInfoEvent;
+
 import org.gwtbootstrap3.client.ui.Panel;
 import org.gwtbootstrap3.client.ui.PanelBody;
 import org.gwtbootstrap3.client.ui.PanelHeader;
+import org.gwtbootstrap3.client.ui.Well;
+import org.gwtbootstrap3.client.ui.constants.WellSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,81 +34,139 @@ import java.util.Map;
  */
 public class BusMonitorPanel extends FlowPanel {
 
-    private final Map<Integer, Map<Integer, BusAddressItemPanel>> addressItemMapping = Maps.newHashMap();
-    private RemoteEventListener listener;
-    private RemoteEventListener connectionListener;
-    private List<Panel> busPanels = new ArrayList<>();
+	private final Map<Integer, Map<Integer, BusAddressItemPanel>> addressItemMapping = Maps
+			.newHashMap();
+	private RemoteEventListener listener;
+	private RemoteEventListener connectionListener;
+	private List<Panel> busPanels = new ArrayList<>();
+	final Well well = new Well();
+	final Label lbl = new Label();
 
-    public BusMonitorPanel() {
-        super();
+	public BusMonitorPanel() {
+		super();
+		lbl.setText("Sorry, there is no connection.Please connect.");
+		lbl.getElement().getStyle().setFloat(Float.NONE);
+		lbl.getElement().getStyle().setColor("Black");
+		well.setSize(WellSize.LARGE);
+		well.add(lbl);
+		well.setHeight("70px");
+		well.setWidth("360px");
+		well.getElement().getStyle().setMarginLeft(480, Unit.PX);
+		well.getElement().getStyle().setMarginTop(280, Unit.PX);
+		
+		setHeight("100%");
+		for (int i = 0; i < 2; i++) {
+			Panel panel = new org.gwtbootstrap3.client.ui.Panel();
+			panel.setWidth("48%");
+			panel.getElement().getStyle().setFloat(Style.Float.LEFT);
+			panel.getElement().getStyle().setMargin(1, Style.Unit.PCT);
 
-        setHeight("100%");
-        getElement().getStyle().setOverflowY(Style.Overflow.SCROLL);
+			PanelHeader panelHeader = new PanelHeader();
+			panelHeader.setText("Bus " + i);
+			PanelBody panelBody = new PanelBody();
+			panel.add(panelHeader);
+			panel.add(panelBody);
+			busPanels.add(panel);
 
-        for (int i = 0; i < 2; i++) {
-            Panel panel = new org.gwtbootstrap3.client.ui.Panel();
-            panel.setWidth("48%");
-            panel.getElement().getStyle().setFloat(Style.Float.LEFT);
-            panel.getElement().getStyle().setMargin(1, Style.Unit.PCT);
+			Map<Integer, BusAddressItemPanel> addressItemPanelMap = Maps
+					.newHashMap();
 
-            PanelHeader panelHeader = new PanelHeader();
-            panelHeader.setText("Bus " + i);
-            PanelBody panelBody = new PanelBody();
-            panel.add(panelHeader);
-            panel.add(panelBody);
-            busPanels.add(panel);
+			for (int j = 0; j < 127; j++) {
+				BusAddressItemPanel busAddressItemPanel = new BusAddressItemPanel(
+						i, j);
+				busAddressItemPanel.getElement().getStyle()
+						.setPaddingRight(5, Style.Unit.PX);
+				busAddressItemPanel.getElement().getStyle()
+						.setPaddingBottom(5, Style.Unit.PX);
+				panelBody.add(busAddressItemPanel);
 
-            Map<Integer, BusAddressItemPanel> addressItemPanelMap = Maps.newHashMap();
+				addressItemPanelMap.put(j, busAddressItemPanel);
+			}
+			addressItemMapping.put(i, addressItemPanelMap);
+		}
 
-            for (int j = 0; j < 127; j++) {
-                BusAddressItemPanel busAddressItemPanel = new BusAddressItemPanel(j);
-                busAddressItemPanel.getElement().getStyle().setPaddingRight(5, Style.Unit.PX);
-                busAddressItemPanel.getElement().getStyle().setPaddingBottom(5, Style.Unit.PX);
-                panelBody.add(busAddressItemPanel);
+		listener = new RemoteEventListener() {
+			public void apply(Event anEvent) {
+				BusDataEvent busDataEvent = (BusDataEvent) anEvent;
+				addressItemMapping.get(busDataEvent.getBus())
+						.get(busDataEvent.getAddress())
+						.updateData(busDataEvent.getData());
+			}
+		};
+		connectionListener = new RemoteEventListener() {
+			@Override
+			public void apply(Event event) {
+				DeviceInfoEvent deviceInfoEvent = (DeviceInfoEvent) event;
 
-                addressItemPanelMap.put(j, busAddressItemPanel);
-            }
-            addressItemMapping.put(i, addressItemPanelMap);
-        }
+				if (deviceInfoEvent.getEventType() == DeviceInfoEvent.TYPE.CONNECTED) {
 
-        listener = new RemoteEventListener() {
-            public void apply(Event anEvent) {
-                BusDataEvent busDataEvent = (BusDataEvent) anEvent;
-                addressItemMapping.get(busDataEvent.getBus()).get(busDataEvent.getAddress()).updateData(busDataEvent.getData());
-            }
-        };
-        connectionListener = new RemoteEventListener() {
-            @Override
-            public void apply(Event event) {
-                DeviceInfoEvent deviceInfoEvent = (DeviceInfoEvent) event;
-                if (deviceInfoEvent.getEventType() == DeviceInfoEvent.TYPE.CONNECTED) {
-                    ServiceUtils.getBusService().startTrackingBus(new EmptyCallback<Void>());
-                } else if (deviceInfoEvent.getEventType() == DeviceInfoEvent.TYPE.DISCONNECTED) {
-                    ServiceUtils.getBusService().stopTrackingBus(new EmptyCallback<Void>());
-                }
-            }
-        };
-    }
+					ServiceUtils.getBusService().startTrackingBus(
+							new EmptyCallback<Void>());
+					remove(well);
+					onLoad();
 
-    @Override
-    protected void onLoad() {
-        super.onLoad();
-        for (Panel busPanel : busPanels) {
-            add(busPanel);
-        }
-        ServiceUtils.getBusService().startTrackingBus(new EmptyCallback<Void>());
-        EventReceiver.getInstance().addListener(BusDataEvent.class, listener);
-        EventReceiver.getInstance().addListener(DeviceInfoEvent.class, connectionListener);
-    }
+				} else if (deviceInfoEvent.getEventType() == DeviceInfoEvent.TYPE.DISCONNECTED) {
+					ServiceUtils.getBusService().stopTrackingBus(
+							new EmptyCallback<Void>());
+					removePanel();
+					add(well);
+				}
+			}
+		};
+	}
 
-    @Override
-    protected void onUnload() {
-        super.onUnload();
-        ServiceUtils.getBusService().stopTrackingBus(new EmptyCallback<Void>());
-        EventReceiver.getInstance().removeListener(BusDataEvent.class, listener);
-        EventReceiver.getInstance().removeListener(DeviceInfoEvent.class, connectionListener);
-        for (Panel busPanel : busPanels) {
-            remove(busPanel);
-        }
-    }
+	@Override
+	protected void onLoad() {
+		super.onLoad();
+		EventReceiver.getInstance().addListener(DeviceInfoEvent.class,
+				connectionListener);
+
+		ServiceUtils.getBusService().isBusConnected(
+				new AsyncCallback<Boolean>() {
+					@Override
+					public void onFailure(Throwable caught) {
+					}
+
+					@Override
+					public void onSuccess(Boolean result) {
+						if (result) {
+
+							remove(well);
+							addPanel();
+
+						} else {
+							removePanel();
+							add(well);
+						}
+					}
+				});
+
+		ServiceUtils.getBusService()
+				.startTrackingBus(new EmptyCallback<Void>());
+		EventReceiver.getInstance().addListener(BusDataEvent.class, listener);
+
+	}
+
+	@Override
+	protected void onUnload() {
+		super.onUnload();
+		ServiceUtils.getBusService().stopTrackingBus(new EmptyCallback<Void>());
+		EventReceiver.getInstance()
+				.removeListener(BusDataEvent.class, listener);
+		EventReceiver.getInstance().removeListener(DeviceInfoEvent.class,
+				connectionListener);
+		removePanel();
+	}
+
+	public void removePanel() {
+		for (Panel busPanel : busPanels) {
+			remove(busPanel);
+		}
+	}
+
+	public void addPanel() {
+		for (Panel busPanel : busPanels) {
+			add(busPanel);
+		}
+	}
 }

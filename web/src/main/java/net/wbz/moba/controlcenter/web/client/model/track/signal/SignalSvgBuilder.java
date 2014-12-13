809ebb5c553b2c3b85132ab20e42cdcc31938fc1
Daniel Tuerk <daniel.tuerk@w-b-z.com>
2014-12-13 16:18:11 +0100

    update signal SVG

--- web/src/main/java/net/wbz/moba/controlcenter/web/client/model/track/signal/SignalSvgBuilder.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/client/model/track/signal/SignalSvgBuilder.java
@@ -21,13 +21,34 @@ public class SignalSvgBuilder {
     private final static float PREVIEW_WIDTH = 50f;
     private final static float PREVIEW_HEIGHT = 120f;
 
+    public static final String SHORTCUT_RED_1 = "r1";
+    public static final String SHORTCUT_RED_2 = "r2";
+    public static final String SHORTCUT_GREEN_1 = "g1";
+    public static final String SHORTCUT_GREEN_2 = "g2";
+    public static final String SHORTCUT_WHITE = "w";
+    public static final String SHORTCUT_YELLOW_1 = "y1";
+    public static final String SHORTCUT_YELLOW_2 = "y2";
+
     private SignalSvgBuilder() {
     }
 
+    /**
+     * Return the instance of the singleton.
+     *
+     * @return {@link net.wbz.moba.controlcenter.web.client.model.track.signal.SignalSvgBuilder} instance
+     */
     public static SignalSvgBuilder getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * Adding the svg content for the {@link net.wbz.moba.controlcenter.web.shared.track.model.Signal.TYPE} to the
+     * given {@link org.vectomatic.dom.svg.OMSVGSVGElement}.
+     *
+     * @param signalType {@link net.wbz.moba.controlcenter.web.shared.track.model.Signal.TYPE}
+     * @param doc        {@link org.vectomatic.dom.svg.OMSVGDocument}
+     * @param svg        {@link org.vectomatic.dom.svg.OMSVGSVGElement}
+     */
     public void addSvgContent(Signal.TYPE signalType, OMSVGDocument doc, OMSVGSVGElement svg) {
         switch (signalType) {
             case BLOCK:
@@ -50,6 +71,14 @@ public class SignalSvgBuilder {
         svg.appendChild(SvgTrackUtil.createCircle(doc, 12.5f, 19f, 5f, colorActive));
     }
 
+    /**
+     * Adding the preview svg by {@link net.wbz.moba.controlcenter.web.shared.track.model.Signal.TYPE} to the given
+     * {@link org.vectomatic.dom.svg.OMSVGSVGElement}.
+     *
+     * @param signalType {@link net.wbz.moba.controlcenter.web.shared.track.model.Signal.TYPE}
+     * @param doc        {@link org.vectomatic.dom.svg.OMSVGDocument}
+     * @param svg        {@link org.vectomatic.dom.svg.OMSVGSVGElement}
+     */
     public void addPreview(Signal.TYPE signalType, OMSVGDocument doc, OMSVGSVGElement svg) {
         float circleLeftX = PREVIEW_PADDING_LEFT + 12.5f;
         float textRightX = PREVIEW_PADDING_LEFT + 52f;
@@ -62,11 +91,11 @@ public class SignalSvgBuilder {
 
                 // red light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 100f, circleWidth, SVGConstants.CSS_RED_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textLeftX, 105f, (short) 5, "rt"));
+                svg.appendChild(doc.createSVGTextElement(textLeftX, 105f, (short) 5, SHORTCUT_RED_1));
 
                 // green light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleRightX, 100f, circleWidth, SVGConstants.CSS_GREEN_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textRightX, 105f, (short) 5, "gr"));
+                svg.appendChild(doc.createSVGTextElement(textRightX, 105f, (short) 5, SHORTCUT_GREEN_1));
 
                 break;
             case ENTER:
@@ -74,15 +103,15 @@ public class SignalSvgBuilder {
 
                 // red light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 100f, circleWidth, SVGConstants.CSS_RED_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textLeftX, 105f, (short) 5, "rt"));
+                svg.appendChild(doc.createSVGTextElement(textLeftX, 105f, (short) 5, SHORTCUT_RED_1));
 
                 // yellow light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleRightX, 100f, circleWidth, SVGConstants.CSS_YELLOW_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textRightX, 105f, (short) 5, "ye"));
+                svg.appendChild(doc.createSVGTextElement(textRightX, 105f, (short) 5, SHORTCUT_YELLOW_1));
 
                 // green light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleRightX, 18f, circleWidth, SVGConstants.CSS_GREEN_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textRightX, 18f, (short) 5, "gr"));
+                svg.appendChild(doc.createSVGTextElement(textRightX, 18f, (short) 5, SHORTCUT_GREEN_1));
 
                 break;
             case EXIT:
@@ -90,25 +119,25 @@ public class SignalSvgBuilder {
 
                 // red 1 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 40f, circleWidth, SVGConstants.CSS_RED_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textLeftX, 45f, (short) 5, "rt"));
+                svg.appendChild(doc.createSVGTextElement(textLeftX, 45f, (short) 5, SHORTCUT_RED_1));
                 // red 2 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleRightX, 40f, circleWidth, SVGConstants.CSS_RED_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textRightX, 45f, (short) 5, "rt"));
+                svg.appendChild(doc.createSVGTextElement(textRightX, 45f, (short) 5, SHORTCUT_RED_2));
 
                 // white light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleRightX, 60f, circleWidth, SVGConstants.CSS_WHITE_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textRightX, 65f, (short) 5, "w"));
+                svg.appendChild(doc.createSVGTextElement(textRightX, 65f, (short) 5, SHORTCUT_WHITE));
 
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 78f, circleWidth, SVGConstants.CSS_WHITE_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textLeftX, 84f, (short) 5, "w"));
+                svg.appendChild(doc.createSVGTextElement(textLeftX, 84f, (short) 5, SHORTCUT_WHITE));
 
                 // yellow light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 100f, circleWidth, SVGConstants.CSS_YELLOW_VALUE));
-                svg.appendChild(doc.createSVGTextElement(0, 105f, (short) 5, "ye"));
+                svg.appendChild(doc.createSVGTextElement(0, 105f, (short) 5, SHORTCUT_YELLOW_1));
 
                 // green light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, circleLeftX, 18f, circleWidth, SVGConstants.CSS_GREEN_VALUE));
-                svg.appendChild(doc.createSVGTextElement(textLeftX, 18f, (short) 5, "gr"));
+                svg.appendChild(doc.createSVGTextElement(textLeftX, 18f, (short) 5, SHORTCUT_GREEN_1));
 
                 break;
             case BEFORE:
@@ -132,19 +161,19 @@ public class SignalSvgBuilder {
 
                 // yellow 1 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, PREVIEW_PADDING_LEFT + 45, 25f, circleWidth, SVGConstants.CSS_YELLOW_VALUE));
-                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 5f, 28f, (short) 5, "y1"));
+                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 5f, 28f, (short) 5, SHORTCUT_YELLOW_1));
 
                 // green 1 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, PREVIEW_PADDING_LEFT + 45, 65f, circleWidth, SVGConstants.CSS_GREEN_VALUE));
-                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 70f, 68f, (short) 5, "gr1"));
+                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 70f, 68f, (short) 5, SHORTCUT_GREEN_1));
 
                 // yellow 2 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, PREVIEW_PADDING_LEFT + 15, 65f, circleWidth, SVGConstants.CSS_YELLOW_VALUE));
-                svg.appendChild(doc.createSVGTextElement(0, 68f, (short) 5, "y2"));
+                svg.appendChild(doc.createSVGTextElement(0, 68f, (short) 5, SHORTCUT_YELLOW_2));
 
                 // green 2 light
                 svg.appendChild(SvgTrackUtil.createCircle(doc, PREVIEW_PADDING_LEFT + 75, 25f, circleWidth, SVGConstants.CSS_GREEN_VALUE));
-                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 95, 28f, (short) 5, "gr2"));
+                svg.appendChild(doc.createSVGTextElement(PREVIEW_PADDING_LEFT + 95, 28f, (short) 5, SHORTCUT_GREEN_2));
                 break;
         }
     }

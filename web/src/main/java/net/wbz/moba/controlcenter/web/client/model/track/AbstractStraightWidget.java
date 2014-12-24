9e911e6b40b629d4e0c7d78bcad3f88cb26d275d
Daniel Tuerk <daniel.tuerk@w-b-z.com>
2014-12-24 10:48:11 +0100

    bugfix direction

--- web/src/main/java/net/wbz/moba/controlcenter/web/client/model/track/AbstractStraightWidget.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/client/model/track/AbstractStraightWidget.java
@@ -17,14 +17,12 @@ abstract public class AbstractStraightWidget extends AbstractBlockSvgTrackWidget
         svg.appendChild(SvgTrackUtil.createRectangle(getSvgDocument(), 0f, 10f, 25f, 5f));
     }
 
-//    @Override
-//    public TrackPart getTrackPart(Widget containerWidget, int zoomLevel) {
-//        Straight straight = new Straight();
-//        straight.setDirection(getStraightDirection());
-//        straight.setGridPosition(getGridPosition(containerWidget, zoomLevel));
-//        straight.setFunctionConfigs(getStoredWidgetFunctionConfigs());
-//        return straight;
-//    }
+    @Override
+    public Straight getNewTrackPart() {
+        Straight straight = new Straight();
+        straight.setDirection(getStraightDirection());
+        return straight;
+    }
 
     abstract public Straight.DIRECTION getStraightDirection();
 

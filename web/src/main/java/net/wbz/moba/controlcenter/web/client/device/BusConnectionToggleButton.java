cdf38d2dfe764b7fe6797c68f7f1d28e2691517f
Daniel Tuerk <daniel.tuerk@w-b-z.com>
2014-12-25 11:12:57 +0100

    quick fix to avoid fire events of value changed for bus connection toggle button

--- web/src/main/java/net/wbz/moba/controlcenter/web/client/device/BusConnectionToggleButton.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/client/device/BusConnectionToggleButton.java
@@ -6,7 +6,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import net.wbz.moba.controlcenter.web.client.ServiceUtils;
 import net.wbz.moba.controlcenter.web.client.util.EmptyCallback;
 import net.wbz.moba.controlcenter.web.client.util.Log;
-import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.extras.toggleswitch.client.ui.ToggleSwitch;
 import org.gwtbootstrap3.extras.toggleswitch.client.ui.base.constants.ColorType;
 
@@ -18,34 +17,56 @@ import org.gwtbootstrap3.extras.toggleswitch.client.ui.base.constants.ColorType;
  */
 public class BusConnectionToggleButton extends ToggleSwitch {
 
+    /**
+     * Quick fix {@see BusConnectionToggleButton#setValue}.
+     */
+    private boolean avoidFireEvent = false;
+
     public BusConnectionToggleButton(final DeviceListBox deviceListBox) {
         super();
         setLabelText("Bus");
         setOffColor(ColorType.DANGER);
         setOnColor(ColorType.SUCCESS);
-//        setOnIcon(IconType.FLASH);
-//        setOffIcon(IconType.FLASH);
         addValueChangeHandler(new ValueChangeHandler<Boolean>() {
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> booleanValueChangeEvent) {
-                if (booleanValueChangeEvent.getValue()) {
-                    ServiceUtils.getBusService().changeDevice(deviceListBox.getSelectedDevice(), new AsyncCallback<Void>() {
-                        @Override
-                        public void onFailure(Throwable caught) {
-                            Log.error("change device " + caught.getMessage());
-                        }
+                if (!avoidFireEvent) {
+                    if (booleanValueChangeEvent.getValue()) {
+                        ServiceUtils.getBusService().changeDevice(deviceListBox.getSelectedDevice(), new AsyncCallback<Void>() {
+                            @Override
+                            public void onFailure(Throwable caught) {
+                                Log.error("change device " + caught.getMessage());
+                            }
 
-                        @Override
-                        public void onSuccess(Void result) {
-                            ServiceUtils.getBusService().connectBus(new EmptyCallback<Void>());
+                            @Override
+                            public void onSuccess(Void result) {
+                                ServiceUtils.getBusService().connectBus(new EmptyCallback<Void>());
+                            }
+                        });
+                    } else {
+                        {
+                            ServiceUtils.getBusService().disconnectBus(new EmptyCallback<Void>());
                         }
-                    });
-                } else {
-                    {
-                        ServiceUtils.getBusService().disconnectBus(new EmptyCallback<Void>());
                     }
+                } else {
+                    // activate for next time
+                    avoidFireEvent=false;
                 }
+
             }
         });
+    }
+
+    /**
+     * Quick fix for the fire events state. Bootstrap lib also fire the value change event if the
+     * parameter is {@code false}.
+     *
+     * @param value value to set
+     * @param fireEvents do not fire events for value changed
+     */
+    @Override
+    public void setValue(Boolean value, boolean fireEvents) {
+        avoidFireEvent=!fireEvents;
+        super.setValue(value, fireEvents);
     }
 }

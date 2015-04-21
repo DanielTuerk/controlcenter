e337119e938f2cac780a0eab6a3567cdf82f7ac4
Daniel Tuerk <daniel.tuerk@w-b-z.com>
2015-04-21 10:36:44 +0200

    handle invalid train address and add "removeAll" train listeners

--- web/src/main/java/net/wbz/moba/controlcenter/web/server/train/TrainManager.java
+++ web/src/main/java/net/wbz/moba/controlcenter/web/server/train/TrainManager.java
@@ -1,6 +1,5 @@
 package net.wbz.moba.controlcenter.web.server.train;
 
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.persist.Transactional;
@@ -17,7 +16,7 @@ import javax.inject.Provider;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.util.List;
-import java.util.Map;
+
 
 /**
  * Manager to access the {@link net.wbz.moba.controlcenter.web.shared.train.Train}s from database.
@@ -30,87 +29,23 @@ import java.util.Map;
 @Singleton
 public class TrainManager {
 
-    private final Map<Long, Train> trains = Maps.newConcurrentMap();
-
     private final Provider<EntityManager> entityManager;
+    private final EventBroadcaster eventBroadcaster;
+    private final DeviceManager deviceManager;
 
     @Inject
     public TrainManager(final EventBroadcaster eventBroadcaster,
                         final DeviceManager deviceManager, Provider<EntityManager> entityManager) {
         this.entityManager = entityManager;
+        this.eventBroadcaster = eventBroadcaster;
+        this.deviceManager = deviceManager;
 
         deviceManager.addDeviceConnectionListener(new DeviceConnectionListener() {
             @Override
             public void connected(Device device) {
                 try {
                     for (final Train train : getTrains()) {
-                        deviceManager.getConnectedDevice().getTrainModule(
-                                (byte) train.getAddress()).addTrainDataListener(
-                                new TrainDataListener() {
-                                    @Override
-                                    public void drivingLevelChanged(int i) {
-                                        train.setDrivingLevel(i);
-                                        eventBroadcaster.fireEvent(new TrainDrivingLevelEvent(train.getId(), i));
-                                    }
-
-                                    @Override
-                                    public void drivingDirectionChanged(TrainModule.DRIVING_DIRECTION driving_direction) {
-                                        train.setDrivingDirection(Train.DIRECTION.valueOf(driving_direction.name()));
-                                        eventBroadcaster.fireEvent(new TrainDrivingDirectionEvent(train.getId(),
-                                                TrainDrivingDirectionEvent.DRIVING_DIRECTION.valueOf(
-                                                        driving_direction.name())
-                                        ));
-                                    }
-
-                                    @Override
-                                    public void functionStateChanged(byte functionAddress, int functionBit, boolean active) {
-                                        TrainFunction.FUNCTION trainFunction;
-                                        switch (functionBit) {
-                                            case 1:
-                                                trainFunction = TrainFunction.FUNCTION.F1;
-                                                break;
-                                            case 2:
-                                                trainFunction = TrainFunction.FUNCTION.F2;
-                                                break;
-                                            case 3:
-                                                trainFunction = TrainFunction.FUNCTION.F3;
-                                                break;
-                                            case 4:
-                                                trainFunction = TrainFunction.FUNCTION.F4;
-                                                break;
-                                            case 5:
-                                                trainFunction = TrainFunction.FUNCTION.F5;
-                                                break;
-                                            case 6:
-                                                trainFunction = TrainFunction.FUNCTION.F6;
-                                                break;
-                                            case 7:
-                                                trainFunction = TrainFunction.FUNCTION.F7;
-                                                break;
-                                            case 8:
-                                                trainFunction = TrainFunction.FUNCTION.F8;
-                                                break;
-                                            default:
-                                                throw new RuntimeException("no function available for bit: " + functionBit);
-                                        }
-                                        train.getFunction(trainFunction).setState(active);
-                                        eventBroadcaster.fireEvent(new TrainFunctionStateEvent(train.getId(), trainFunction,
-                                                active));
-                                    }
-
-                                    @Override
-                                    public void lightStateChanged(boolean state) {
-                                        train.getFunction(TrainFunction.FUNCTION.LIGHT).setState(state);
-                                        eventBroadcaster.fireEvent(new TrainLightStateEvent(train.getId(), state));
-                                    }
-
-                                    @Override
-                                    public void hornStateChanged(boolean state) {
-                                        train.getFunction(TrainFunction.FUNCTION.HORN).setState(state);
-                                        eventBroadcaster.fireEvent(new TrainHornStateEvent(train.getId(), state));
-                                    }
-                                }
-                        );
+                        reregisterConsumer(train, deviceManager, eventBroadcaster);
                     }
                 } catch (DeviceAccessException e) {
                     e.printStackTrace();
@@ -125,11 +60,88 @@ public class TrainManager {
 
     }
 
-    public Train getTrain(long id) {
-        if (trains.containsKey(id)) {
-            return trains.get(id);
+    private void reregisterConsumer(final Train train, DeviceManager deviceManager, final EventBroadcaster eventBroadcaster) throws DeviceAccessException {
+        if (train.getAddress() >= 0) {
+            TrainModule trainModule = deviceManager.getConnectedDevice().getTrainModule((byte) train.getAddress());
+            trainModule.removeAllTrainDataListeners();
+            trainModule.addTrainDataListener(
+                    new TrainDataListener() {
+                        @Override
+                        public void drivingLevelChanged(int i) {
+                            train.setDrivingLevel(i);
+                            eventBroadcaster.fireEvent(new TrainDrivingLevelEvent(train.getId(), i));
+                        }
+
+                        @Override
+                        public void drivingDirectionChanged(TrainModule.DRIVING_DIRECTION driving_direction) {
+                            train.setDrivingDirection(Train.DIRECTION.valueOf(driving_direction.name()));
+                            eventBroadcaster.fireEvent(new TrainDrivingDirectionEvent(train.getId(),
+                                    TrainDrivingDirectionEvent.DRIVING_DIRECTION.valueOf(
+                                            driving_direction.name())
+                            ));
+                        }
+
+                        @Override
+                        public void functionStateChanged(byte functionAddress, int functionBit, boolean active) {
+                            TrainFunction.FUNCTION trainFunction;
+                            switch (functionBit) {
+                                case 1:
+                                    trainFunction = TrainFunction.FUNCTION.F1;
+                                    break;
+                                case 2:
+                                    trainFunction = TrainFunction.FUNCTION.F2;
+                                    break;
+                                case 3:
+                                    trainFunction = TrainFunction.FUNCTION.F3;
+                                    break;
+                                case 4:
+                                    trainFunction = TrainFunction.FUNCTION.F4;
+                                    break;
+                                case 5:
+                                    trainFunction = TrainFunction.FUNCTION.F5;
+                                    break;
+                                case 6:
+                                    trainFunction = TrainFunction.FUNCTION.F6;
+                                    break;
+                                case 7:
+                                    trainFunction = TrainFunction.FUNCTION.F7;
+                                    break;
+                                case 8:
+                                    trainFunction = TrainFunction.FUNCTION.F8;
+                                    break;
+                                default:
+                                    throw new RuntimeException("no function available for bit: " + functionBit);
+                            }
+                            train.getFunction(trainFunction).setState(active);
+                            eventBroadcaster.fireEvent(new TrainFunctionStateEvent(train.getId(), trainFunction,
+                                    active));
+                        }
+
+                        @Override
+                        public void lightStateChanged(boolean state) {
+                            train.getFunction(TrainFunction.FUNCTION.LIGHT).setState(state);
+                            eventBroadcaster.fireEvent(new TrainLightStateEvent(train.getId(), state));
+                        }
+
+                        @Override
+                        public void hornStateChanged(boolean state) {
+                            train.getFunction(TrainFunction.FUNCTION.HORN).setState(state);
+                            eventBroadcaster.fireEvent(new TrainHornStateEvent(train.getId(), state));
+                        }
+                    }
+            );
         }
-        throw new RuntimeException(String.format("no train found for id %d", id));
+    }
+
+    public Train getTrain(long id) {
+        Query typedQuery = entityManager.get().createQuery(
+                "SELECT x FROM Train x where id=:id");
+        typedQuery.setParameter("id", id);
+        Train train = (Train) typedQuery.getSingleResult();
+        if (train != null) {
+            return train;
+        }
+        throw new RuntimeException(String.format("no train for id %d found!", id));
     }
 
     @Transactional
@@ -139,6 +151,12 @@ public class TrainManager {
             train = manager.merge(train);
         }
         manager.persist(train);
+
+        try {
+            reregisterConsumer(train, deviceManager, eventBroadcaster);
+        } catch (DeviceAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Train> getTrains() {

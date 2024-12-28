ec29fa4a7dbfdd9af69c39ba1c970849ad743b07
Daniel Tuerk <daniel.tuerk@jambit.com>
2024-12-28 20:25:28 +0100

    add header with menu

--- frontend/src/app/control-center/control-center.routes.ts
+++ frontend/src/app/control-center/control-center.routes.ts
@@ -2,6 +2,9 @@ import {Routes} from "@angular/router";
 import {TrainComponent} from "./train/train.component";
 import {TrainEditComponent} from "./train/train-edit/train-edit.component";
 import {ScenarioComponent} from "./scenario/scenario.component";
+import {ViewerComponent} from "./viewer/viewer.component";
+import {EditorComponent} from "./editor/editor.component";
+import {SettingComponent} from "./setting/setting.component";
 
 export const routes: Routes = [
   {
@@ -15,5 +18,17 @@ export const routes: Routes = [
   {
     path: 'scenario',
     component: ScenarioComponent
+  },
+  {
+    path: 'viewer',
+    component: ViewerComponent
+  },
+  {
+    path: 'editor',
+    component: EditorComponent
+  },
+  {
+    path: 'settings',
+    component: SettingComponent
   }
 ];
--- frontend/src/app/control-center/header/header.component.ts
+++ frontend/src/app/control-center/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatMiniFabButton} from "@angular/material/button";
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-header',
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbar,
+    MatIcon,
+    MatDivider,
+    MatButton,
+    MatMiniFabButton
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
